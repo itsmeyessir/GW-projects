f986fd5b6414b4fb587645e8b6ff32a2fba52868
Robbie Espaldon <100555644+itsmeyessir@users.noreply.github.com>
2024-04-28 12:27:44 +0800

    Add files via upload

---
+++ login-register/login.php
@@ -0,0 +1,72 @@
+<?php
+session_start();
+if (isset($_SESSION["user"])) {
+   header("Location: index.php");
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Form</title>
+    <link href='https://fonts.googleapis.com/css?family=Lexend' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Comfie.ai</h1>
+    <div class="container">
+        <?php
+        if (isset($_POST["login"])) {
+           $username = $_POST["username"];
+           $password = $_POST["password"];
+            require_once "database.php";
+            $sql = "SELECT * FROM users WHERE username = '$username'";
+            $result = mysqli_query($conn, $sql);
+            $user = mysqli_fetch_array($result, MYSQLI_ASSOC);
+            if ($user) {
+                if (password_verify($password, $user["password"])) {
+                    session_start();
+                    $_SESSION["user"] = "yes";
+                    header("Location: index.php");
+                    die();
+                }else{
+                    echo "<div class='alert alert-danger'>Password does not match</div>";
+                }
+            }else{
+                echo "<div class='alert alert-danger'>Username does not match</div>";
+            }
+        }
+        ?>
+        <div class="form-box">
+            <h2>Login</h2>
+            <form action="login.php" method="post">
+                <div class="form-group">
+                    <input type="username" placeholder="Username" name="username" class="form-control">
+                </div>
+                <div class="form-group">
+                    <input type="password" placeholder="Password" name="password" class="form-control">
+                </div>
+                <div class="form-btn">
+                    <input type="submit" value="Login" name="login" class="btn btn-primary">    
+                </div>
+            </form>
+            <div><p>Not registered yet <a href="registration.php">Register Here</a></p></div>
+        </div> 
+      <!-- <form action="login.php" method="post">
+        <div class="form-group">
+            <input type="username" placeholder="Username" name="username" class="form-control">
+        </div>
+        <div class="form-group">
+            <input type="password" placeholder="Password" name="password" class="form-control">
+        </div>
+        <div class="form-btn">
+            <input type="submit" value="Login" name="login" class="btn btn-primary">
+        </div>
+      </form>
+     <div><p>Not registered yet? <a href="registration.php">Register Here</a></p></div> -->
+    </div>
+</body>
+</html>
